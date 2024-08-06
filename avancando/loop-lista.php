2cbf2a8c206360c6cc63c1f1126246a17ff69de6
Pamella Siqueira <contatopamellasiqueira@gmail.com>
2024-08-06 16:39:15 -0300

    Uso de arrays e loop for

--- avancando/loop-lista.php
+++ avancando/loop-lista.php
@@ -2,6 +2,7 @@
 
 //array - uma sequencia de informações normalmente do mesmo tipo
 $idadeList = [21, 23, 19, 25, 20, 41, 18];
+$alunos = ['João', 'Maria', 'Pedro', 'Ana'];
 
 //conta quantos intens tem no meu array
 //echo count($idadeList);
@@ -10,4 +11,8 @@ $idadeList = [21, 23, 19, 25, 20, 41, 18];
 //incrementa até 7
 for ($i = 0; $i < count($idadeList); $i++) {
     echo $idadeList[$i]. PHP_EOL; //PHP_EOL exibe quebrando a linha
+}
+
+for($i = 0; $i < count($alunos); $i++) {
+    echo 'Aluno: '. $alunos[$i]. PHP_EOL;
 }
