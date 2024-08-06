ef5bf0b0e89026c8569b3e4280de2ecd8ceba5a2
Pamella Siqueira <contatopamellasiqueira@gmail.com>
2024-08-06 16:14:34 -0300

    Uso de arrays em php

---
+++ avancando/listas.php
@@ -0,0 +1,9 @@
+<?php
+
+//array - uma sequencia de informações normalmente do mesmo tipo
+$idadeList = [21, 23, 19, 25, 20, 41, 18];
+//para manipular um dado do array, basta informar o seu índice
+$primeiraIdade = $idadeList[0];
+
+//exibir o dado com base no índice informado
+echo $primeiraIdade;
