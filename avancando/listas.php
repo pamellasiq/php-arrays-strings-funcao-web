97af35701a7c9ec825e598343048d9a792796c4d
Pamella Siqueira <contatopamellasiqueira@gmail.com>
2024-08-06 22:45:16 -0300

    adicionar dados num array

--- avancando/listas.php
+++ avancando/listas.php
@@ -3,7 +3,11 @@
 //array - uma sequencia de informações normalmente do mesmo tipo
 $idadeList = [21, 23, 19, 25, 20, 41, 18];
 //para manipular um dado do array, basta informar o seu índice
-$primeiraIdade = $idadeList[0];
+
+//se o indice não for informado, o dado é adicionado no último índice informado
+$idadeList[] = 20;
 
 //exibir o dado com base no índice informado
-echo $primeiraIdade;
+foreach ($idadeList as $idade) {
+    echo $idade.PHP_EOL;
+}
