f1e3ed7316cef76f3de942a33c6a1518667fcc3d
Pamella Siqueira <contatopamellasiqueira@gmail.com>
2024-08-07 18:32:00 -0300

    Uso do PHP com HTML

--- avancando/banco.php
+++ avancando/banco.php
@@ -31,12 +31,39 @@ titularComLetrasMaiusculas($contasCorrentes['123.456.789-10']);
 // unset($contasCorrentes['123.456.789-10']);
 
 //foreach >Para cada uma das contasCorrentes, chamada de conta.
-foreach ($contasCorrentes as $cpf => $conta){
-    exibeMensagem(
-        mensagem: "-------------------------------------------
-        CPF: {$cpf}
-        Titular: {$conta['titular']}
-        Cargo: {$conta['cargo']}
-        Saldo: R$ {$conta['saldo']}
-------------------------------------------- ");
-}
+// foreach ($contasCorrentes as $cpf => $conta){
+//     exibeMensagem(
+//         mensagem: "-------------------------------------------
+//         CPF: {$cpf}
+//         Titular: {$conta['titular']}
+//         Cargo: {$conta['cargo']}
+//         Saldo: R$ {$conta['saldo']}
+// ------------------------------------------- ");
+// }
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas correntes</h1>
+
+    <dl>
+        <?php foreach($contasCorrentes as $cpf => $conta) { ?>
+        <dt>
+            <h3><?= $conta['titular']; ?> - <?= $cpf; ?></h3>
+        </dt>
+        <dd>
+            Saldo: <?= $conta['saldo']; ?>
+        </dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
